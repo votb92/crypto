fad64e19c9a9fa05ba9fc3659d7b122b9f97e46b
votb92 <votb@mymail.vcu.edu>
2021-09-16 17:37:01 -0400

    add API access

---
+++ app/controllers/concerns/secret.rb
@@ -0,0 +1,6 @@
+class Secret
+  @@API_Key = '14c1f2e4-fff3-4319-979b-f024adc98a2c'
+  def self.key
+    @@API_Key
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    load "secret.rb"
+    key = Secret.key
+    require 'net/http'
+    require 'json'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100'
+    @uri = URI(@url)
+    params = { :CMC_PRO_API_KEY => key }
+    @uri.query = URI.encode_www_form(params)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @mycoin = ["BTC", "XRP", "ADA", "XLM", "STEEM"]
   end
 end
